8bc279e3a7d543ec81437d8a9b487195cdf4eac6
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-06-13 20:28:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-//your code here
+// your code here
+const inputElement = document.getElementById('newTodoInput');
+const buttonElement = document.getElementById('addTodoBtn');
+const todoHeading = document.getElementById('todoList');
+
+
+buttonElement.addEventListener("click",function(){
+	const task = inputElement.value;
+
+	if(task === "") return;
+	const listItem = document.createElement('li');
+	listItem.textContent=task
+
+
+
+todoHeading.appendChild(listItem);
+inputElement.value="";
+})
+
+
+
+
+
+
+
+
+
+
 
